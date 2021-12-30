5aeff8503e3c8e227e5c40069cc965efb9437eb3
Santiago Lo Coco <santilococo.01@gmail.com>
2021-12-30 20:49:56 -0300

    Add dynamic calculations of width and height (from CocoASAIS)

--- scripts/common.sh
+++ scripts/common.sh
@@ -23,6 +23,36 @@ useWhiptail() {
     whiptail "$@" 0 0
 }
 
+calcWidthAndRun() {
+    width=$(echo "$@" | grep -oP '(?<=").*?(?=")' | wc -c)
+    comm=$(echo "$@" | sed "s/WIDTH/$((${width}+8))/g")
+    if [[ $comm != *"3>&1 1>&2 2>&3" ]]; then
+        comm="${comm} 3>&1 1>&2 2>&3"
+    fi
+    commOutput=$(eval $comm)
+    exitStatus=$?
+    [ ! -z $commOutput ] && echo $commOutput
+    return $exitStatus
+}
+
+calcHeightAndRun() {
+    str=$(echo "$@" | grep -oP '(?<=").*?(?=")')
+    newlines=$(printf "$str" | grep -c $'\n')
+    chars=$(echo "$str" | wc -c)
+    height=$(echo "$chars" "$newlines" | awk '{
+        x = (($1 - $2 + ($2 * 60)) / 60)
+        printf "%d", (x == int(x)) ? x : int(x) + 1
+    }')
+    comm=$(echo "$@" | sed "s/HEIGHT/$((5+$height))/g")
+    if [[ $comm != *"3>&1 1>&2 2>&3" ]]; then
+        toRun="${comm} 3>&1 1>&2 2>&3"
+    fi
+    commOutput=$(eval $comm)
+    exitStatus=$?
+    [ ! -z $commOutput ] && echo $commOutput
+    return $exitStatus
+}
+
 setDialogBox() {
     export dialogBox=${1}
 }
