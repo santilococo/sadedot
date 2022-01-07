2b8c200c0033dd0b5b72773f1cb3543751118486
Santiago Lo Coco <santilococo.01@gmail.com>
2022-01-06 23:49:40 -0300

    Update shebang

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 displayDialogBox() {
     case $dialogBox in
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 cloneAndMake() {
     displayDialogBox --infobox "Downloading ${1}" VALUES
