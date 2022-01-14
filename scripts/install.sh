9a08be907deb90adb55455942ed1ac272e35d913
Santiago Lo Coco <santilococo.01@gmail.com>
2022-01-14 01:30:19 -0300

    Run shellcheck (install.sh)

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,13 +2,13 @@
 
 cloneAndMake() {
     displayDialogBox --infobox "Downloading ${1}" VALUES
-    git clone $2 2>&1 | debug
-    (cd $1; sudo make install 2>&1 | debug)
+    git clone "$2" 2>&1 | debug
+    (cd "$1" || { echo "Couldn't cd into '$1'." 1>&2 && exit 1; }; sudo make install 2>&1 | debug)
 }
 
 downloadAndInstallPackages() {
-    DOTFILES_CONFIG=$HOME/.config
-    cd $DOTFILES_CONFIG
+    DOTFILES_CONFIG="$HOME/.config"
+    cd "$DOTFILES_CONFIG" || { echo "Couldn't cd into '$DOTFILES_CONFIG'." 1>&2 && exit 1; }
 
     if [[ ! -d "dwmblocks" ]]; then
         cloneAndMake "dwmblocks" "https://github.com/santilococo/dwmblocks.git"
