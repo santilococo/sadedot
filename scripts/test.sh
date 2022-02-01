c9e085d5ac06b402a41e5b76a8938249f70e0413
Santiago Lo Coco <santilococo.01@gmail.com>
2022-02-01 18:08:29 -0300

    Remove test.sh

--- scripts/test.sh
+++
@@ -1,5 +0,0 @@
-source sadedot/scripts/common.sh
-setDialogBox "dialog"
-            msg="\nFile already exists: $(basename "$1"), what would you like to do?"
-            options=(1 "Skip" 2 "Skip ALL" 3 "Overwrite" 4 "Overwrite all" 5 "Backup" 6 "Backup all")
-            selectedOption=$(displayDialogBox --menu "$msg" VALUES 0 "${options[@]}" 3>&1 1>&2 2>&3)
