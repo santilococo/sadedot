f90f0027b0dc7ee0e22124128ba75ec9e71bfe63
Santiago Lo Coco <santilococo.01@gmail.com>
2022-02-08 01:15:27 -0300

    Refactor update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-git submodule update --remote --merge
-gitStatus=$(git status --porcelain)
-grep -q "sadedot" <(echo "$gitStatus") || return
-git commit -m "Update sadedot submodule" sadedot
-git push
+runScript() {
+    git submodule update --remote --merge
+    gitStatus=$(git status --porcelain)
+    grep -q "sadedot" <(echo "$gitStatus") || return
+    git commit -m "Update sadedot submodule" sadedot
+    git push
+}
+
+runScript
