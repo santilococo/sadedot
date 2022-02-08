902b1b13857a02bd310d6b6cf96c6d9655066d9a
Santiago Lo Coco <santilococo.01@gmail.com>
2022-02-08 00:19:05 -0300

    Fix bugs

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-git submodule update
-# git submodule foreach git pull
-# git commit -m "Update sadedot submodule" sadedot
-# git push
+git submodule update --remote --merge
+git commit -m "Update sadedot submodule" sadedot
+git push
