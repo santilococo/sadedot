e79c5dbdf15cc6455ff023e34b4b20e3b03d4d80
Santiago Lo Coco <santilococo.01@gmail.com>
2022-02-08 00:53:07 -0300

    Add conditions in update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 git submodule update --remote --merge
+gitStatus=$(git status --porcelain)
+grep -q "sadedot" <(echo $gitStatus) || return
 git commit -m "Update sadedot submodule" sadedot
 git push
